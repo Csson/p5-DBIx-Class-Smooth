1f5de107de6cc67c78451fb2cebacc6da53351e5
Erik Carlsson <info@code301.com>
2018-11-10 18:39:42 +0000

    confess rather than die

--- lib/DBIx/Class/Smooth/Lookup/DateTime/datepart.pm
+++ lib/DBIx/Class/Smooth/Lookup/DateTime/datepart.pm
@@ -9,17 +9,18 @@ package DBIx::Class::Smooth::Lookup::DateTime::datepart;
 our $VERSION = '0.0101';
 
 use parent 'DBIx::Class::Smooth::ResultSet::Base';
+use Carp qw/confess/;
 use experimental qw/signatures postderef/;
 
 sub smooth__lookup__datepart($self, $column_name, $value, $params, @rest) {
     if(scalar $params->@* != 1) {
-        die sprintf 'substring expects exactly one params, got <%s>', join (', ' => $params->@*);
+        confess sprintf 'substring expects exactly one params, got <%s>', join (', ' => $params->@*);
     }
-
     my @secure_params = grep { /^[a-z_]+$/i } $params->@*;
     if(scalar @secure_params != scalar $params->@*) {
         die sprintf 'datepart got faulty params: <%s>', join (', ' => $params->@*);
     }
+
     my $complete = $self->dt_SQL_pluck({ -ident => $column_name }, $params->[0]);
 
     return { left_hand_function => { complete => $complete->$*->[0] }, value => $value };
