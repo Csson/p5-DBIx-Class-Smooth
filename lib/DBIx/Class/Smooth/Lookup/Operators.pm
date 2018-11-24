3dcf414120ebc3d7b02971b4160cad2bf1c1bf62
Erik Carlsson <info@code301.com>
2018-11-24 11:13:20 +0000

    Add more operators

--- lib/DBIx/Class/Smooth/Lookup/Operators.pm
+++ lib/DBIx/Class/Smooth/Lookup/Operators.pm
@@ -9,6 +9,10 @@ package DBIx::Class::Smooth::Lookup::Operators;
 our $VERSION = '0.0101';
 
 use parent qw/
+    DBIx::Class::Smooth::Lookup::Operators::gt
+    DBIx::Class::Smooth::Lookup::Operators::gte
+    DBIx::Class::Smooth::Lookup::Operators::lt
+    DBIx::Class::Smooth::Lookup::Operators::lte
     DBIx::Class::Smooth::Lookup::Operators::in
     DBIx::Class::Smooth::Lookup::Operators::like
     DBIx::Class::Smooth::Lookup::Operators::not_in
